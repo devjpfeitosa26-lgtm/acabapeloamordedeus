e65f264472f6fd0236f01db6cd51d93701d5c9e6
devjpfeitosa26-lgtm <devjpfeitosa26@gmail.com>
2026-08-20 18:01:19 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,24 +1,12 @@
 public class Usuario {
+    private final Email email;
+    private final Senha senha;
 
-    private String email; // puxa a classe email
-    private String senha; // puxa a classe senha
-
-    public Usuario(String email, String senha) { //cria um vetor para fazer a verificação do usuário
-        if (email == null || email.isBlank()) { // condição se o campo de email está vazio ou em branco
-            throw new IllegalArgumentException("Email não pode ser vazio");
-        }
-        if (senha == null || senha.length() 12) { //condição para a senha ter mínimo 12 caracteres 
-            throw new IllegalArgumentException("Senha deve ter pelo menos 12 caracteres");
-        }
+    public Usuario(Email email, Senha senha) {
         this.email = email;
         this.senha = senha;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
+    public Email getEmail() { return email; }
+    public Senha getSenha() { return senha; }
 }
