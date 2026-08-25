f4cf56dcd8f9d6ebed1519268501134b7d39bdda
Notliw-Matos <notliw.projetos@gmail.com>
2026-08-24 22:00:26 -0300

    Criacao dos eventos

--- Evento.java
+++ Evento.java
@@ -2,13 +2,50 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Evento{
-    private String localPalestra;
-    private String tituloPalestra;
+    private String localEvento;
+    private String tituloEvento;
     private int capacidadeDeInscritos;
-    private LocalDateTime dataEvento;
+    private LocalDateTime dataHoraEvento;
 
-    public Evento(String localPalestra, String tituloPalestra, int capacidadeDeInscritos, LocalDateTime dataEvento){
+    // contrutor
+    public Evento(String localEvento, String tituloEvento, int capacidadeDeInscritos, LocalDateTime dataHoraEvento){
+        
 
+        if(localEvento == null || localEvento.isBlank()){
+            throw new IllegalArgumentException("O local do Evento é obrigatorio.");
+        }
+
+        if(tituloEvento == null || tituloEvento.isBlank()){
+            throw new IllegalArgumentException("O título do evento não pode ser vazio.");
+        }
+
+        if(dataHoraEvento == null){
+            throw new IllegalArgumentException("A data e o horario são obrigatorios.");
+        }
+
+        if(capacidadeDeInscritos <= 0){
+            throw new IllegalArgumentException("A capacidade do evento deve ser maior que zero.");
+        }
+
+        this.localEvento = localEvento;
+        this.tituloEvento = tituloEvento;
+        this.dataHoraEvento = dataHoraEvento;
+        this.capacidadeDeInscritos = capacidadeDeInscritos;
+
+    }
+
+
+    public String getLocalPalestra() {
+        return localEvento;
+    }
+    public String getTituloPalestra() {
+        return tituloEvento;
+    }
+    public int getCapacidadeDeInscritos() {
+        return capacidadeDeInscritos;
+    }
+    public LocalDateTime getDataHoraEvento() {
+        return dataHoraEvento;
     }
 
     // validar disponibilidade
